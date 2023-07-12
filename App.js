89184ea46bc07f1de31877723f1dd290badcf4ec
Zachary Folwick <zfolwick@gmail.com>
2023-07-12 13:31:51 -0700

    added text input

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput>Zach Folwick</TextInput>
       <Text>Welcome to CI/CD example!</Text>
       <StatusBar style="auto" />
     </View>
