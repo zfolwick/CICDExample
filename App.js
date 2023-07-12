0792d539a6b05a63b92d0d77b2b94e79ede0a57c
Zachary Folwick <zfolwick@gmail.com>
2023-07-12 16:20:51 -0700

    made a component and started using it

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Greetings from "./components/greetings"
+
+// import Greetings from './components/greetings';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TextInput>Zach Folwick</TextInput>
-      <Text>Welcome to CI/CD example!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <Greetings />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/greetings.js
@@ -0,0 +1,37 @@
+import { React, Component } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native"
+// import { Text, View } from "react-native"
+
+
+export default class Greetings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: ''}
+    }
+
+    render() {
+        return (
+            // <View>
+            //     <Text>Hello World!</Text>
+            // </View>
+            <View style={styles.container}>
+              <TextInput style={{height:30, borderColor: 'gray', borderWidth: 1}}
+              placeholder="write your name"
+              onChangeText={(name) => this.setState({name})} />
+              <Text style={{marginTop: 10}}>Welcome to CI/CD example, {this.state.name}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40,
+        marginHorizontal: 16,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
+
